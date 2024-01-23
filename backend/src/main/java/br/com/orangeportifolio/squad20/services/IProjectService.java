4cae23fbf232e7eaf4b55027c2967fcf052c71b3
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-01-23 19:05:16 -0300

    Update
    - Criação do metodo save na interface IUserService;
    - Criação dos metodos CRUD na interface  IProjectService;
    - Criação da classe UserServiceImpl;

--- backend/src/main/java/br/com/orangeportifolio/squad20/services/IProjectService.java
+++ backend/src/main/java/br/com/orangeportifolio/squad20/services/IProjectService.java
@@ -1,5 +1,14 @@
 package br.com.orangeportifolio.squad20.services;
 
-public interface IProjectService {
+import java.util.List;
 
+import br.com.orangeportifolio.squad20.model.Project;
+
+public interface IProjectService {
+	
+	public boolean save(Project novo);
+	public boolean update(Project Project, Integer id);
+	public List<Project> findAll();
+	public List<Project> findByTag(String nome);
+	public boolean delete(Integer id);
 }
--- backend/src/main/java/br/com/orangeportifolio/squad20/services/UserServiceImpl.java
+++ backend/src/main/java/br/com/orangeportifolio/squad20/services/UserServiceImpl.java
@@ -2,6 +2,8 @@ package br.com.orangeportifolio.squad20.services;
 
 import br.com.orangeportifolio.squad20.model.User;
 
-public interface IUserService {
-	public Boolean save(User user);
+public class UserServiceImpl implements IUserService{
+
+	
+
 }
