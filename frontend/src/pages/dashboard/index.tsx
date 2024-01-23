755ff254bec843e3107ac0d297491c4a1a62de61
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-01-23 17:40:17 -0300

    feat: criando dashboard

---
+++ frontend/src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export function Dashboard() {
+   return (
+      <div className={styles.container}>
+         <h1>Dashboard</h1>
+      </div>
+   );
+}
