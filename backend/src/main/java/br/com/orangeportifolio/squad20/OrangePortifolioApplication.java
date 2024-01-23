cd0058a95671247af219db6a0becbb967b2a2660
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-01-22 21:48:34 -0300

    Primeiro Commit do Backend
    - Criação do Projeto com Spring Boot e suas dependencias;

---
+++ backend/src/main/java/br/com/orangeportifolio/squad20/OrangePortifolioApplication.java
@@ -0,0 +1,13 @@
+package br.com.orangeportifolio.squad20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangePortifolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangePortifolioApplication.class, args);
+	}
+
+}
