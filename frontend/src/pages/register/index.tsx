bdcb6ec476f3d9b0efab3b436028d46a96b3f17c
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-01-23 17:22:12 -0300

    feat: criando tela de cadastro

---
+++ frontend/src/pages/register/index.tsx
@@ -0,0 +1,73 @@
+import { Input } from "../../components/input";
+import styles from "./styles.module.css";
+import ImgCadastro from "../../assets/imgcadastro.png";
+import { Button } from "../../components/button";
+import { FormEvent, useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+export function Register() {
+   const [senha, setSenha] = useState("");
+   const [mostrarSenha, setMostrarSenha] = useState(false);
+
+   const handleInputChange = (e: FormEvent) => {
+      // @ts-expect-error valor do state
+      setSenha(e.target.value);
+   };
+
+   const handleToggleSenha = () => {
+      setMostrarSenha(!mostrarSenha);
+   };
+
+   return (
+      <div className={styles.container}>
+         <div className={styles.div_img}>
+            <img src={ImgCadastro} alt="imagem cadastro" />
+         </div>
+         <div className={styles.content}>
+            <h3>Cadastre-se</h3>
+
+            <div className={styles.form}>
+               <form>
+                  <div className={styles.name}>
+                     <Input
+                        label="Nome*"
+                        type="text"
+                        placeholder="Digite seu nome"
+                        required
+                     />
+                     <Input
+                        label="Sobrenome*"
+                        type="text"
+                        placeholder="Digite seu sobrenome"
+                        required
+                     />
+                  </div>
+                  <Input
+                     label="Email address"
+                     type="email"
+                     placeholder="Digite seu email"
+                     required
+                  />
+                  <Input
+                     label="Password"
+                     type={mostrarSenha ? "text" : "password"}
+                     name="senha"
+                     value={senha}
+                     placeholder="Digite sua senha"
+                     onChange={handleInputChange}
+                     required
+                  />
+                  <button
+                     className={styles.button_view}
+                     type="button"
+                     onClick={handleToggleSenha}
+                  >
+                     {mostrarSenha ? <FaEyeSlash /> : <FaEye />}
+                  </button>
+                  <Button className={styles.btnRegister}>CADASTRAR</Button>
+               </form>
+            </div>
+         </div>
+      </div>
+   );
+}
