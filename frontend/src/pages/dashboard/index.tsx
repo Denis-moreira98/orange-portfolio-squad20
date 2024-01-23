25d99b852ecf302a7c31ac1f035987e16b8a93dc
brenddamachado <brenda16machado.bm@gmail.com>
2024-01-23 19:32:59 -0300

    criando o perfil do dashboard

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,9 +1,15 @@
 import styles from "./styles.module.css";
+import log from "../../assets/imgcadastro.png"
 
 export function Dashboard() {
    return (
       <div className={styles.container}>
-         <h1>Dashboard</h1>
+         <div className="">
+            <img src={log} alt="" />
+            <h2>Camila Soares</h2>
+            <p>Brasil</p>
+            <button type="button">ADICIONAR PROJETO</button>
+         </div>
       </div>
    );
 }
