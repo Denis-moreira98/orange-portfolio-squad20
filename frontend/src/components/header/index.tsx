d0125cd607a8481497c8e528d4fc7dbc846e9786
queziafiladelfo <queziafiladelfo@gmail.com>
2024-01-23 23:16:52 -0300

    Adiciona estilização do header desktop

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,9 +1,37 @@
 import styles from "./styles.module.css";
+import orangeLogo from '../../assets/logo-orange.png';
+import imagePerfil from '../../assets/image-perfil.png';
+import iconNot from '../../assets/icon-notifications.png';
 
 export function Header() {
    return (
-      <div className={styles.container}>
-         <header>header</header>
-      </div>
+      <header className={styles.container}>         
+            <div className={styles.divLinks}>
+               
+               <img src={orangeLogo} 
+                     alt="Logo Orange Portifólio" 
+                     className={styles.headerLogo} />
+
+               <div>
+                  <a href="#">Meus projetos</a>
+                  <a href="#">Descobrir</a>
+               </div>
+            </div>
+
+            <div className={styles.divRitgh}>
+               <a href="#">
+                  <img src={imagePerfil} 
+                        alt="Imagem do Perfil" 
+                        className={styles.imgPerfil} />
+               </a>
+               <a href="#">
+                  <img src={iconNot} 
+                           alt="Icone de notificação" 
+                           className={styles.imgNot} />
+               </a>
+               
+            </div>          
+         </header>
+      
    );
 }
