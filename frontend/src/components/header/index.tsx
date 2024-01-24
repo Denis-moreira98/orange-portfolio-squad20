ab89775120d1cd751e52bf2f93a4e5317b32ea85
queziafiladelfo <queziafiladelfo@gmail.com>
2024-01-24 04:22:04 -0300

    feat: Adiciona responsividade ao header

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -2,23 +2,29 @@ import styles from "./styles.module.css";
 import orangeLogo from '../../assets/logo-orange.png';
 import imagePerfil from '../../assets/image-perfil.png';
 import iconNot from '../../assets/icon-notifications.png';
+import iconMenu from '../../assets/Icon-menu.svg';
 
 export function Header() {
    return (
-      <header className={styles.container}>         
-            <div className={styles.divLinks}>
-               
+      <>
+      <header className={styles.container}>
+
+         <div className={styles.divRigth}> 
+            <div className={styles.container__menu}>
+               <img src={iconMenu} alt="ìcone do menu" className={styles.icon__menu}/> 
                <img src={orangeLogo} 
-                     alt="Logo Orange Portifólio" 
-                     className={styles.headerLogo} />
+                  alt="Logo Orange Portifólio" 
+                  className={styles.headerLogo} />
+            </div>              
+            <nav className={styles.menu}>
+                  <ul>
+                     <li><a href="#">Meus projetos</a></li>
+                     <li><a href="#">Descobrir</a></li>
+                  </ul>                
+            </nav>
+         </div>        
 
-               <div>
-                  <a href="#">Meus projetos</a>
-                  <a href="#">Descobrir</a>
-               </div>
-            </div>
-
-            <div className={styles.divRitgh}>
+         <div className={styles.divRitgh}>
                <a href="#">
                   <img src={imagePerfil} 
                         alt="Imagem do Perfil" 
@@ -28,10 +34,9 @@ export function Header() {
                   <img src={iconNot} 
                            alt="Icone de notificação" 
                            className={styles.imgNot} />
-               </a>
-               
-            </div>          
-         </header>
-      
+               </a>               
+         </div>          
+      </header>
+      </>
    );
 }
