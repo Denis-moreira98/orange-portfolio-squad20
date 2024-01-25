5072f76179d72a3c7767d247f5f24bee9d225c48
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-01-24 21:17:45 -0300

    feat: finalizando component Modal

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,71 +1,86 @@
 import styles from "./styles.module.css";
-import orangeLogo from '../../assets/logo-orange.png';
-import imagePerfil from '../../assets/image-perfil.png';
+import orangeLogo from "../../assets/logo-orange.png";
+import imagePerfil from "../../assets/image-perfil.png";
 
 import { IoMenuSharp } from "react-icons/io5";
 import { IoIosNotifications } from "react-icons/io";
 
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export function Header() {
-   const [menuOpen, setMenuOpen] = useState <boolean>(true);
-   useEffect( () => {
-      window.addEventListener('resize', () => {
-         if(window.innerWidth >= 800) {
-            setMenuOpen(false)
+   const [menuOpen, setMenuOpen] = useState<boolean>(true);
+   useEffect(() => {
+      window.addEventListener("resize", () => {
+         if (window.innerWidth >= 800) {
+            setMenuOpen(false);
          }
-      })
-   }, [])
+      });
+   }, []);
 
    return (
       <>
-      <div >
+         <div>
+            <header className={styles.header__container}>
+               <div className={styles.divRigth}>
+                  <div className={styles.container__menu}>
+                     <IoMenuSharp
+                        className={styles.icon__menu}
+                        onClick={() => {
+                           setMenuOpen(!menuOpen);
+                        }}
+                     />
+                     <img
+                        src={orangeLogo}
+                        alt="Logo Orange Portifólio"
+                        className={styles.header__logo}
+                     />
+                  </div>
+                  <nav className={styles.menu}>
+                     <ul>
+                        <li>
+                           <a href="#">Meus projetos</a>
+                        </li>
+                        <li>
+                           <a href="#">Descobrir</a>
+                        </li>
+                     </ul>
+                  </nav>
+               </div>
 
-      <header className={styles.header__container}>
-         <div className={styles.divRigth}> 
-            <div className={styles.container__menu}>                  
-               <IoMenuSharp className={styles.icon__menu} onClick={()=> { setMenuOpen(!menuOpen)}} />   
-               <img src={orangeLogo} 
-                    alt="Logo Orange Portifólio" 
-                    className={styles.header__logo} />
-            </div>              
-            <nav className={styles.menu}>
+               <div className={styles.div__left}>
+                  <a href="#">
+                     <img
+                        src={imagePerfil}
+                        alt="Imagem do Perfil"
+                        className={styles.img__perfil}
+                     />
+                  </a>
+                  <IoIosNotifications className={styles.img__notifications} />
+               </div>
+            </header>
+         </div>
+
+         {menuOpen && (
+            <nav className={styles.dropdown}>
                <ul>
-                  <li><a href="#">Meus projetos</a></li>
-                  <li><a href="#">Descobrir</a></li>
-               </ul>                
-            </nav>
-         </div>        
+                  <li>
+                     <a href="#">Meus projetos</a>
+                  </li>
+                  <li>
+                     <a href="#">Descobrir</a>
+                  </li>
+                  <li className={styles.linha__menu}>
+                     <a href="#">Configurações</a>
+                  </li>
+                  <li></li>
+               </ul>
 
-         <div className={styles.div__left}>
-               <a href="#">
-                  <img src={imagePerfil} 
-                       alt="Imagem do Perfil" 
-                       className={styles.img__perfil} />
-               </a>
-               <IoIosNotifications className={styles.img__notifications}/>
-               
-         </div>          
-      </header>
-      </div>
-
-      {menuOpen && (
-         <nav className={styles.dropdown} >
-            <ul>
-               <li><a href="#">Meus projetos</a></li>
-               <li><a href="#">Descobrir</a></li>
-               <li className={styles.linha__menu}><a href="#" >Configurações</a></li>
-               <li></li>  
-            </ul>                
-
-            {/* <div><a href="#">Meus projetos</a></div>
+               {/* <div><a href="#">Meus projetos</a></div>
             <div><a href="#">Descobrir</a></div>
             <div><a href="#">Configurações</a></div>
             <div></div> */}
-            
-         </nav>
-      )}
-
+            </nav>
+         )}
       </>
    );
 }
--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -5,6 +5,7 @@ import { FaImages } from "react-icons/fa";
 import { ModalAddProject } from "../../components/modal";
 
 import Modal from "react-modal";
+import InputModal from "../../components/modal/input";
 
 export function Dashboard() {
    const [modalVisible, setModalVisible] = useState(false);
@@ -24,18 +25,22 @@ export function Dashboard() {
                <img
                   className={styles.fotoperfil}
                   src="https://s3-alpha-sig.figma.com/img/0b59/6e07/52e969b0f04cacb1f6ff5ba506b04f1c?Expires=1707091200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pzGS-mTalYHFx6gxtH8vHGGFRYsIy7-LbQ8J5K9Z6orB3MdAqRJZl04irgdTeVyzKKR2KRPmQ39xALSJYOMAkI4VV6onO8gkcKdCe60~Qhq9HKDIW5rUsHYd8qMZSm3Mdo659TDqsJiSZPQ4~kERmMsvR90GjLuXyMquviS3ZA6QAxkDpcWmA1prQO7QyQa7hNRUdBLDHUKspDLK6Z9v9-ddV8y0jVVBJkRMDPXnXLo7tN-ycmPmsVKL6CHfW8n0-9Qzfmzljxgypr~CTG3wb8pVjhKxEKj-zL1r1psb30-axOhK~mce0-pysqEVZzwoT9kd89ts4NPoaD-qaauRWQ__"
-                  alt=""
+                  alt="perfil"
                />
                <div className={styles.informacoes}>
                   <p className={styles.nome}>Camila Soares</p>
                   <p className={styles.pais}>Brasil</p>
-                  <button className={styles.buttondashboard} type="button">
+                  <button
+                     className={styles.buttondashboard}
+                     onClick={handleOpenModal}
+                     type="button"
+                  >
                      ADICIONAR PROJETO
                   </button>
                </div>
             </div>
             <p className={styles.projetos}>Meus projetos</p>
-            <input
+            <InputModal
                className={styles.pesquisa}
                type="search"
                name=""
@@ -44,7 +49,10 @@ export function Dashboard() {
             />
             <div className={styles.containerproj}>
                <div className={styles.adicionar}>
-                  <button className={styles.buttondashboardd} onClick={handleOpenModal}>
+                  <button
+                     className={styles.buttondashboardd}
+                     onClick={handleOpenModal}
+                  >
                      <FaImages />
                   </button>
 
@@ -55,7 +63,7 @@ export function Dashboard() {
                      Compartilhe seu talento com milhares de pessoas
                   </span>
                </div>
-             {/*   <div className={styles.complemento}></div>
+               {/*   <div className={styles.complemento}></div>
 
                <div className={styles.complemento}></div> */}
             </div>
