0084ac619675c9a030bddb4c3c3b02fd23d09e2f
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-01-24 23:31:44 -0300

    Update IProjectService.java

--- backend/src/main/java/br/com/orangeportifolio/squad20/services/IProjectService.java
+++ backend/src/main/java/br/com/orangeportifolio/squad20/services/IProjectService.java
@@ -3,12 +3,15 @@ package br.com.orangeportifolio.squad20.services;
 import java.util.List;
 
 import br.com.orangeportifolio.squad20.model.Project;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public interface IProjectService {
 	
-	public Project create(Project novo);
-	public boolean update(Project Project, Integer id);
+	public Project create(@Valid @NotNull Project novo);
+	public boolean update(@Valid @NotNull Project project, @NotNull @Positive Integer id);
 	public List<Project> findAll();
 	public List<Project> findByTags(String nome);
-	public boolean delete(Integer id);
+	public boolean delete(@NotNull @Positive Integer id);
 }
