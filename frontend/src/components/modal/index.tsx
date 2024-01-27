3c5bb3dfba5744ca7fc2790c6007e2e121d8cea3
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-01-27 07:19:00 -0300

    style: deixando modal formulário responsivo

--- frontend/src/components/modal/index.tsx
+++ frontend/src/components/modal/index.tsx
@@ -1,5 +1,4 @@
 import Modal from "react-modal";
-import { FiX } from "react-icons/fi";
 import styles from "./styles.module.css";
 
 import { Button } from "../button";
@@ -7,12 +6,27 @@ import InputModal, { TextArea } from "./input/index";
 import { ChangeEvent, useState } from "react";
 import { FaImages } from "react-icons/fa";
 
+// import { ModalView } from "../modalView";
+
 interface ModalProps {
    isOpen: boolean;
    onRequestClose: () => void;
 }
 
 export function ModalAddProject({ isOpen, onRequestClose }: ModalProps) {
+   // const [modalVisible, setModalVisible] = useState(false);
+
+   // function handleOpenModal(e: FormEvent) {
+   //    e.preventDefault();
+
+   //    setTimeout(() => {
+   //       setModalVisible(true);
+   //    }, 800);
+   // }
+   // function handleCloseModal() {
+   //    setModalVisible(false);
+   // }
+
    const [avatarUrl, setAvatarUrl] = useState("");
    // eslint-disable-next-line @typescript-eslint/no-unused-vars
    const [imageAvatar, setImageAvatar] = useState(null);
@@ -43,73 +57,83 @@ export function ModalAddProject({ isOpen, onRequestClose }: ModalProps) {
          backgroundColor: "#FEFEFE",
       },
    };
-
+   Modal.setAppElement("#root");
    return (
-      <Modal
-         isOpen={isOpen}
-         onRequestClose={onRequestClose}
-         style={customStyles}
-      >
-         <div className={styles.div_container}>
-            <h4>Adicionar Projeto</h4>
-            <button
-               type="button"
-               onClick={onRequestClose}
-               className="react-modal-close"
-               style={{
-                  backgroundColor: "transparent",
-                  border: 0,
-                  cursor: "pointer",
-               }}
-            >
-               <FiX size={30} color="#ff5522" />
-            </button>
-         </div>
-         <form>
-            <div className={styles.content}>
-               <div className={styles.div_text}>
-                  <p>Selecione o conteúdo que você deseja fazer upload</p>
+      <>
+         <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            style={customStyles}
+         >
+            <div className={styles.div_container}>
+               <h4>Adicionar Projeto</h4>
+               <form>
+                  <div className={styles.content}>
+                     <div className={styles.div_text}>
+                        <div className={styles.div_file}>
+                           <p>
+                              Selecione o conteúdo que você deseja fazer upload
+                           </p>
+                           <label className={styles.labelAvatar}>
+                              <span>
+                                 <FaImages size={35} color="#323232" />
+                              </span>
+                              <input
+                                 type="file"
+                                 accept="image/png, image/jpeg"
+                                 onChange={handleFile}
+                              />
 
-                  <div className={styles.div_file}>
-                     <label className={styles.labelAvatar}>
-                        <span>
-                           <FaImages size={35} color="#323232" />
-                        </span>
-                        <input
-                           type="file"
-                           accept="image/png, image/jpeg"
-                           onChange={handleFile}
-                        />
-
-                        {avatarUrl && (
-                           <img
-                              className={styles.preview}
-                              src={avatarUrl}
-                              alt="foto do produto"
-                              width={120}
-                              height={100}
-                           />
-                        )}
-                     </label>
+                              {avatarUrl && (
+                                 <img
+                                    className={styles.preview}
+                                    src={avatarUrl}
+                                    alt="foto do produto"
+                                    width={120}
+                                    height={100}
+                                 />
+                              )}
+                           </label>
+                        </div>
+                     </div>
+                     <div className={styles.div_input}>
+                        <InputModal placeholder="Título" />
+                        <InputModal placeholder="Tags" />
+                        <InputModal placeholder="Link" />
+                        <TextArea placeholder="Descrição" />
+                     </div>
                   </div>
-               </div>
-               <div className={styles.div_input}>
-                  <InputModal placeholder="Título" />
-                  <InputModal placeholder="Tags" />
-                  <InputModal placeholder="Link" />
-                  <TextArea placeholder="Descrição" />
-               </div>
+                  <div className={styles.div_button}>
+                     <p>Visualizar publicação</p>
+                     <div className={styles.div_buttons}>
+                        <Button
+                           // onClick={handleOpenModal}
+                           type="submit"
+                           variant="orange"
+                        >
+                           SALVAR
+                        </Button>
+                        <Button
+                           type="button"
+                           onClick={onRequestClose}
+                           className="react-modal-close"
+                           variant="gray"
+                           style={{ color: "#818388" }}
+                        >
+                           CANCELAR
+                        </Button>
+                     </div>
+                  </div>
+               </form>
             </div>
-            <div className={styles.div_button}>
-               <p>Visualizar publicação</p>
-               <div className={styles.div_buttons}>
-                  <Button variant="orange">SALVAR</Button>
-                  <Button variant="gray" style={{ color: "#818388" }}>
-                     CANCELAR
-                  </Button>
-               </div>
-            </div>
-         </form>
-      </Modal>
+         </Modal>
+         {/* {modalVisible && (
+            <ModalView
+               image={avatarUrl}
+               isOpen={modalVisible}
+               onRequestClose={handleCloseModal}
+            />
+         )} */}
+      </>
    );
 }
