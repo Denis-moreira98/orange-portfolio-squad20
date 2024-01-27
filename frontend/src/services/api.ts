2c582ba07d74ca8e6629d812035b76415e62d139
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-01-27 19:06:29 -0300

    feat:  configurando a api com axxios

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosError } from "axios";
+import { parseCookies } from "nookies";
+import { AuthTokenErrors } from "./errors/AuthTokenErrors";
+
+import { signOut } from "../contexts/AuthContext";
+
+export function setupAPIClient(ctx = undefined) {
+   const cookies = parseCookies(ctx);
+
+   const api = axios.create({
+      baseURL: process.env.BASE_URL,
+      headers: {
+         Authorization: `Bearer ${cookies["@Squad20.token"]}`,
+      },
+   });
+
+   api.interceptors.response.use(
+      (response) => {
+         return response;
+      },
+      (error: AxiosError) => {
+         if (error.response.status === 401) {
+            //Qualquer erro 401 (não autorizado) devemos deslogar o usuario
+
+            // eslint-disable-next-line valid-typeof
+            if (typeof window !== undefined) {
+               // chamar a função para deslogar usuario
+               signOut();
+            } else {
+               return Promise.reject(new AuthTokenErrors());
+            }
+         }
+         return Promise.reject(error);
+      }
+   );
+
+   return api;
+}
