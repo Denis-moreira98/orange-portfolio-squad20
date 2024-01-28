7e902de135486c6c92237444e7e7ac3af2e61e93
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-01-27 22:53:53 -0300

    feat: add react-hot-toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,8 +6,11 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 
+import { Toaster } from "react-hot-toast";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
+      <Toaster position="top-right" reverseOrder={false} />
       <AuthProvider>
          <RouterProvider router={router} />
       </AuthProvider>
