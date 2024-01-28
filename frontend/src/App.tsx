6df4452fc27cc309738dd772f403974d79f186c6
brenddamachado <brenda16machado.bm@gmail.com>
2024-01-27 22:17:02 -0300

    criando pastas do homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,32 +2,36 @@ import { Login } from "./pages/login";
 import { Register } from "./pages/register";
 import { Dashboard } from "./pages/dashboard";
 import { NotFound } from "./pages/notFound";
-
+import { Homepage } from "./pages/homepage";
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./components/layout";
 
 const router = createBrowserRouter([
-   {
-      element: <Layout />,
-      children: [
-         {
-            path: "/",
-            element: <Dashboard />,
-         },
-         {
-            path: "*",
-            element: <NotFound />,
-         },
-      ],
-   },
-   {
-      path: "/login",
-      element: <Login />,
-   },
-   {
-      path: "/register",
-      element: <Register />,
-   },
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+  {
+    path: "/inicio",
+    element: <Homepage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
 ]);
 
 export { router };
---
+++ frontend/src/pages/homepage/index.tsx
@@ -0,0 +1,7 @@
+export function Homepage(){
+    return(
+        <>
+        <h1>ahssaisd</h1>
+        </>
+    )
+}
