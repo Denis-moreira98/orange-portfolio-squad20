2ba52c6369cd4e83d287f9f245c6ccede7b08b92
WilbertSilva <wilbertoliveirasilva@gmail.com>
2024-01-29 16:03:02 -0300

    Conexão ao AWS S3
    Novas classes:
    -StorageConfig
    -StorageController
    -IStorageService
    -StorageServiceImpl
    
    Adição de dependências

---
+++ backend/src/main/java/br/com/orangeportifolio/squad20/config/StorageConfig.java
@@ -0,0 +1,32 @@
+package br.com.orangeportifolio.squad20.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class StorageConfig {
+
+	@Value("${aws.access.key.id}")
+	private String accessKey;
+	
+	@Value("${aws.access.key.secret}")
+	private String acessSecret;
+	
+	@Value("${aws.region}")
+	private String region;
+	
+	@Bean
+	public AmazonS3 S3Client() {
+		AWSCredentials credentials=new BasicAWSCredentials(accessKey, acessSecret);
+		return AmazonS3ClientBuilder.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(credentials))
+				.withRegion(region).build();
+	}
+}
---
+++ backend/src/main/java/br/com/orangeportifolio/squad20/controller/StorageController.java
@@ -0,0 +1,46 @@
+package br.com.orangeportifolio.squad20.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.orangeportifolio.squad20.service.storage.StorageServiceImpl;
+
+@RestController
+@RequestMapping("/file")
+public class StorageController {
+
+    @Autowired
+    private StorageServiceImpl service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = service.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<Boolean> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
+}
