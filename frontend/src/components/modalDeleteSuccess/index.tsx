ea9472597b241a24bd1732eafc7b1850eebbd04e
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-01-30 17:49:14 -0300

    feat: criando component ModalDeleteSuccess

---
+++ frontend/src/components/modalDeleteSuccess/index.tsx
@@ -0,0 +1,44 @@
+import { FaCheckCircle } from "react-icons/fa";
+import styles from "./styles.module.css";
+
+import Modal from "react-modal";
+import { Button } from "../button";
+
+interface ModalProps {
+   isOpen: boolean;
+   onRequestClose: () => void;
+}
+
+export function ModalDeleteSuccess({ isOpen, onRequestClose }: ModalProps) {
+   const customStyles = {
+      content: {
+         top: "50%",
+         bottom: "auto",
+         left: "50%",
+         right: "auto",
+         padding: "30px",
+         transform: "translate(-50%, -50%)",
+         backgroundColor: "#FEFEFE",
+      },
+   };
+   return (
+      <Modal
+         isOpen={isOpen}
+         onRequestClose={onRequestClose}
+         style={customStyles}
+      >
+         <div className={styles.container}>
+            <h3>Projeto deletado com sucesso!</h3>
+            <FaCheckCircle color="green" size={40} />
+            <Button
+               type="button"
+               onClick={onRequestClose}
+               className="react-modal-close"
+               variant="orange"
+            >
+               VOLTAR PARA PROJETO
+            </Button>
+         </div>
+      </Modal>
+   );
+}
