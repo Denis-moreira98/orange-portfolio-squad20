c666c4d42fc907ed77d5b630f81c0814fa816891
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-01-30 23:40:58 -0300

    update AuthController
    - Ajuste no metodo Authenticate para retornar um usuário DTO com token;

--- backend/src/main/java/br/com/orangeportifolio/squad20/service/auth/IAuthService.java
+++ backend/src/main/java/br/com/orangeportifolio/squad20/service/auth/IAuthService.java
@@ -2,7 +2,6 @@ package br.com.orangeportifolio.squad20.service.auth;
 
 import br.com.orangeportifolio.squad20.dto.LoginDTO;
 import br.com.orangeportifolio.squad20.dto.UserDTO;
-import br.com.orangeportifolio.squad20.security.OrangeToken;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
