644e5df050400375ca209eae1df5d155b7f0274b
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-01 20:59:35 -0300

    Alteração no retorno
    - Alteração no retorno do método authenticate da classe AuthServiceImpl, para retornar um AuthDTO, e retornar um id, nome, email e o token;

--- backend/src/main/java/br/com/orangeportifolio/squad20/service/auth/IAuthService.java
+++ backend/src/main/java/br/com/orangeportifolio/squad20/service/auth/IAuthService.java
@@ -1,11 +1,11 @@
 package br.com.orangeportifolio.squad20.service.auth;
 
+import br.com.orangeportifolio.squad20.dto.AuthDTO;
 import br.com.orangeportifolio.squad20.dto.LoginDTO;
-import br.com.orangeportifolio.squad20.dto.UserDTO;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public interface IAuthService {
 	
-	public UserDTO authenticate(@Valid @NotNull LoginDTO dataLogin);
+	public AuthDTO authenticate(@Valid @NotNull LoginDTO dataLogin);
 }
