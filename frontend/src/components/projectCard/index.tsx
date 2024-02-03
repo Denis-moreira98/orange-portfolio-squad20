3eabacb2d18f614c9335c67a29a95b4f6d22610a
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-02-03 16:25:02 -0300

    feat: listando projetos na discover

--- frontend/src/components/projectCard/index.tsx
+++ frontend/src/components/projectCard/index.tsx
@@ -2,21 +2,37 @@ import { Label } from "../modalPreview/label";
 import styles from "./styles.module.css";
 import imagePerfil from "../../assets/image-perfil.png";
 
+interface ProjectProps {
+   title: string;
+   midia: string;
+   tags: string[];
+   onClick?: () => void;
+}
 
-export function ProjectCard() {
+export function ProjectCard({ title, midia, tags, onClick }: ProjectProps) {
    return (
-      <figure className={styles.item}>
-         <img src="https://minhasaude.proteste.org.br/wp-content/webp-express/webp-images/uploads/2022/10/muitas-laranjas.png.webp" alt="" className={styles.capa} />
-         <figcaption className={styles.details}>
-            <div className={styles.info}>
-               <img src={imagePerfil} alt="Imagem do usuário" className={styles.imgPerfil} />
-               <span>Bianca Martin</span>
-               <span>02/24</span>
-            </div>
-            <div className={styles.tags}>
-               <Label>UI</Label>
-            </div>
-         </figcaption>
-      </figure>
+      <label onClick={onClick} className={styles.container}>
+         <figure className={styles.item}>
+            <img src={midia} alt={title} className={styles.capa} />
+
+            <figcaption className={styles.details}>
+               <div className={styles.info}>
+                  <img
+                     src={imagePerfil}
+                     alt="Imagem do usuário"
+                     className={styles.imgPerfil}
+                  />
+
+                  <span>Bianca Martin</span>
+                  <span>02/24</span>
+               </div>
+               <div className={styles.tags}>
+                  {tags.map((tag) => (
+                     <Label key={tag}>{tag}</Label>
+                  ))}
+               </div>
+            </figcaption>
+         </figure>
+      </label>
    );
 }
--- frontend/src/pages/discover/index.tsx
+++ frontend/src/pages/discover/index.tsx
@@ -1,33 +1,104 @@
 import InputModal from "../../components/modal/input";
 import styles from "./styles.module.css";
 import { ProjectCard } from "../../components/projectCard";
+import { useEffect, useState } from "react";
+import { setupAPIClient } from "../../services/api";
+import { ModalPreview } from "../../components/modalPreview";
+
+export interface ProjectsProps {
+   idProject: string;
+   title: string;
+   tags: string;
+   linkProject: string;
+   description: string;
+   userId: string | number;
+   midia: string;
+}
 
 export function Discover() {
-    return (
-        <div className={styles.container}>
+   const [projects, setProjects] = useState<ProjectsProps[]>([]);
+   const [openModalPreview, setOpenModalPreview] = useState(false);
+   const [selectedProject, setSelectedProject] = useState<ProjectsProps | null>(
+      null
+   );
+
+   useEffect(() => {
+      async function getProject() {
+         try {
+            const apiClient = setupAPIClient();
+            const response = await apiClient.get("/project/all");
+
+            setProjects(response.data);
+         } catch (err) {
+            console.log(err);
+         }
+      }
+
+      getProject();
+   }, []);
+
+   // Abre o modal e define o projeto selecionado
+   function handleOpenModal(project: ProjectsProps) {
+      setSelectedProject(project);
+      setOpenModalPreview(true);
+   }
+
+   // Fecha o modal e limpa o projeto selecionado
+   function handleCloseModalPreview() {
+      setSelectedProject(null);
+      setOpenModalPreview(false);
+   }
+   return (
+      <>
+         <div className={styles.container}>
             <main>
-                <p className={styles.description}>Junte-se à comunidade de inovação, inspiração e descobertas, transformando experiências em conexões inesquecíveis</p>
+               <p className={styles.description}>
+                  Junte-se à comunidade de inovação, inspiração e descobertas,
+                  transformando experiências em conexões inesquecíveis
+               </p>
 
-                <div className={styles.input__box}>
-                    <div className={styles.div__input}>
-                        <InputModal
-                            className={styles.input__field}
-                            type="search"
-                            name="campoInput"
-                            id="campoInput"
-                            placeholder="Buscar tags "
-                        />
-                        <label htmlFor="campoInput" className={styles.placeholder_label}>Buscar tags </label>
-                    </div>
-                </div>
+               <div className={styles.input__box}>
+                  <div className={styles.div__input}>
+                     <InputModal
+                        className={styles.input__field}
+                        type="search"
+                        name="campoInput"
+                        id="campoInput"
+                        placeholder="Buscar tags "
+                     />
+                     <label
+                        htmlFor="campoInput"
+                        className={styles.placeholder_label}
+                     >
+                        Buscar tags{" "}
+                     </label>
+                  </div>
+               </div>
 
-                <section className={styles.gallery}>
-                    <ProjectCard />
-                    <ProjectCard />
-                    <ProjectCard />
-                    <ProjectCard />
-                </section>
+               <section className={styles.gallery}>
+                  {projects.map((project) => (
+                     <ProjectCard
+                        key={project.idProject}
+                        title={project.title}
+                        midia={project.midia}
+                        tags={project.tags.split(" ")}
+                        onClick={() => handleOpenModal(project)}
+                     />
+                  ))}
+               </section>
             </main>
-        </div>
-    );
+         </div>
+         {openModalPreview && selectedProject && (
+            <ModalPreview
+               title={selectedProject.title}
+               tags={selectedProject.tags.split(" ")}
+               link={selectedProject.linkProject}
+               description={selectedProject.description}
+               image={selectedProject.midia}
+               isOpen={openModalPreview}
+               onRequestClose={handleCloseModalPreview}
+            />
+         )}
+      </>
+   );
 }
