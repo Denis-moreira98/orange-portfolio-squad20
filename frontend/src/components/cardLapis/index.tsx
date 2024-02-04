e75e1cd366c883d85f8431f40fc85dbb24a89235
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-02-04 01:54:15 -0300

    feat: passando props para o componente CardLapis

--- frontend/src/components/cardLapis/index.tsx
+++ frontend/src/components/cardLapis/index.tsx
@@ -5,115 +5,116 @@ import { ModalDelete } from "../modalDelete";
 import { ModalAddProject } from "../modal";
 import { ModalEditProject } from "../../components/modalEdit";
 import { Label } from "../modalPreview/label";
-import imagePerfil from "../../assets/image-perfil.png";
+import imagePerfil from "../../assets/avatarDefault.jpg";
 
-export function CardLapis() {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [menuAberto, setMenuAberto] = useState(false);
-  const [modalDelete, setModalDelete] = useState(false);
-  const [modalEdit, setModalEdit] = useState(false);
-  const [opcaoSelecionada, setOpcaoSelecionada] = useState<string | null>(null);
+interface ProjectProps {
+   idProject: string;
+   userName: string;
+   midia: string;
+   tags: string[];
+}
 
-  const handleIconClick = () => {
-    setMenuAberto(!menuAberto);
-  };
+export function CardLapis({ userName, midia, tags, idProject }: ProjectProps) {
+   const [modalVisible, setModalVisible] = useState(false);
+   const [menuAberto, setMenuAberto] = useState(false);
+   const [modalDelete, setModalDelete] = useState(false);
+   const [modalEdit, setModalEdit] = useState(false);
+   const [opcaoSelecionada, setOpcaoSelecionada] = useState<string | null>(
+      null
+   );
 
-  // Modal Delete
-  const handleDeleteProject = () => {
-    setModalDelete(true);
-    setOpcaoSelecionada("excluir");
-  };
+   const handleIconClick = () => {
+      setMenuAberto(!menuAberto);
+   };
 
-  function handleCloseModalDelete() {
-    setModalDelete(false);
-  }
+   // Modal Delete
+   const handleDeleteProject = () => {
+      setModalDelete(true);
+      setOpcaoSelecionada("excluir");
+   };
 
-  // Modal Edit
-  function handleEditarProject() {
-    setModalEdit(true);
-    setOpcaoSelecionada("editar");
-  }
+   function handleCloseModalDelete() {
+      setModalDelete(false);
+   }
 
-  function handleCloseModalEdit() {
-    setModalEdit(false);
-  }
-  function handleCloseModal() {
-    setModalVisible(false);
-  }
-  return (
-    <>
-      <div className={styles.lapisdashboard} onClick={handleIconClick}>
-        <BiSolidPencil />
-        {menuAberto && (
-          <div
-            className={`${styles.menu} ${
-              opcaoSelecionada === "excluir" ? styles.excluir : ""
-            } ${opcaoSelecionada === "editar" ? styles.editar : ""}`}
-          >
-            <button
-              type="button"
-              onClick={handleEditarProject}
-            >
-              Editar
-            </button>
-            <button
-              type="button"
-              onClick={handleDeleteProject}
-            >
-              Excluir
-            </button>
-          </div>
-        )}
-      </div>
-      <figure className={styles.item}>
-        <img
-          src="https://minhasaude.proteste.org.br/wp-content/webp-express/webp-images/uploads/2022/10/muitas-laranjas.png.webp"
-          alt=""
-          className={styles.capa}
-        />
-        <figcaption className={styles.details}>
-          <div className={styles.info}>
-            <img
-              src={imagePerfil}
-              alt="Imagem do usuário"
-              className={styles.imgPerfil}
+   // Modal Edit
+   function handleEditarProject() {
+      setModalEdit(true);
+      setOpcaoSelecionada("editar");
+   }
+
+   function handleCloseModalEdit() {
+      setModalEdit(false);
+   }
+   function handleCloseModal() {
+      setModalVisible(false);
+   }
+   return (
+      <>
+         <div className={styles.lapisdashboard} onClick={handleIconClick}>
+            <BiSolidPencil />
+            {menuAberto && (
+               <div
+                  className={`${styles.menu} ${
+                     opcaoSelecionada === "excluir" ? styles.excluir : ""
+                  } ${opcaoSelecionada === "editar" ? styles.editar : ""}`}
+               >
+                  <button type="button" onClick={handleEditarProject}>
+                     Editar
+                  </button>
+                  <button type="button" onClick={handleDeleteProject}>
+                     Excluir
+                  </button>
+               </div>
+            )}
+         </div>
+         <figure className={styles.item}>
+            <img src={midia} alt="" className={styles.capa} />
+            <figcaption className={styles.details}>
+               <div className={styles.info}>
+                  <img
+                     src={imagePerfil}
+                     alt="Imagem do usuário"
+                     className={styles.imgPerfil}
+                  />
+                  <div className={styles.dados}>
+                     <span>{userName}</span>
+                     <span>02/24</span>
+                  </div>
+               </div>
+               <div className={styles.tags}>
+                  {tags.map((tag) => (
+                     <Label key={tag}>{tag}</Label>
+                  ))}
+               </div>
+            </figcaption>
+         </figure>
+         {modalVisible && (
+            <ModalAddProject
+               isOpen={modalVisible}
+               onRequestClose={() => setModalVisible(false)}
             />
-            <div className={styles.dados}>
-              {" "}
-              <span>Bianca Martin</span>
-              <span>02/24</span>
-            </div>
-          </div>
-          <div className={styles.tags}>
-            <Label>UI</Label>
-            <Label>UI</Label>
-          </div>
-        </figcaption>
-      </figure>
-      {modalVisible && (
-        <ModalAddProject
-          isOpen={modalVisible}
-          onRequestClose={() => setModalVisible(false)}
-        />
-      )}
-      {modalEdit && (
-        <ModalEditProject
-          isOpen={modalEdit}
-          onRequestClose={handleCloseModalEdit}
-        />
-      )}
-      {modalDelete && (
-        <ModalDelete
-          isOpen={modalDelete}
-          onRequestClose={handleCloseModalDelete}
-        />
-      )}
-      {modalVisible && (
-        <ModalAddProject
-          isOpen={modalVisible}
-          onRequestClose={handleCloseModal}
-        />
-      )}
-    </>
-  );
+         )}
+         {modalEdit && (
+            <ModalEditProject
+               idProject={idProject}
+               isOpen={modalEdit}
+               onRequestClose={handleCloseModalEdit}
+            />
+         )}
+         {modalDelete && (
+            <ModalDelete
+               idProject={idProject}
+               isOpen={modalDelete}
+               onRequestClose={handleCloseModalDelete}
+            />
+         )}
+         {modalVisible && (
+            <ModalAddProject
+               isOpen={modalVisible}
+               onRequestClose={handleCloseModal}
+            />
+         )}
+      </>
+   );
 }
