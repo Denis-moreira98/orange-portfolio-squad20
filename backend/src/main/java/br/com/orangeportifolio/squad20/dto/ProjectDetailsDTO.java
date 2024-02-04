eee64223d09100c8f9a13ac5516f1591e1cb3df0
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-03 22:07:24 -0300

    Create ProjectDetailsDTO.java
    - Criação da interface ProjectDetailsDTO para abrigar e retornar os dados da consulta feita com o JPQL;

---
+++ backend/src/main/java/br/com/orangeportifolio/squad20/dto/ProjectDetailsDTO.java
@@ -0,0 +1,13 @@
+package br.com.orangeportifolio.squad20.dto;
+
+public interface ProjectDetailsDTO {
+    Integer getIdProject();
+    String getTitle();
+    String getTags();
+    String getLinkProject();
+    String getDescription();
+    Integer getUserId();
+    String getUserName();
+    String getMidia();
+}
+
